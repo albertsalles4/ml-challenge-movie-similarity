d97387e6d61b8454e7c1d1da3f0205d5c2dd0b7e
Albert Salles <albert.salles@mosaicfactor.com>
2025-02-16 16:47:34 +0100

    Fix: Correct CSV path

--- app/repository/movie_repository.py
+++ app/repository/movie_repository.py
@@ -5,6 +5,6 @@ from app.model.embeddings.base_embedding import BaseEmbeddingModel
 
 def get_movies_dataset(embedding_model: BaseEmbeddingModel) -> pd.DataFrame:
     """Loads the dataset from the movies.csv file"""
-    df = pd.read_csv("dataset/movies.csv", index_col=0)
-    df["embedding"] = df["overview"].apply(embedding_model.encode_text)
+    df = pd.read_csv("app/repository/dataset/movies.csv", index_col=0)
+    df["embedding"] = df["overview"].apply(lambda x: embedding_model.encode_text(str(x)))
     return df
