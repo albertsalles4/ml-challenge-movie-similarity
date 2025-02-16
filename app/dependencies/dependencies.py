58783ea5212bcc158e9d64cede9b44d7011d003f
Albert Salles <albert.salles@mosaicfactor.com>
2025-02-16 16:47:15 +0100

    Change: Use Word2Vec and Cosine Similarity as default models

--- app/dependencies/dependencies.py
+++ app/dependencies/dependencies.py
@@ -1,18 +1,20 @@
 from fastapi import Depends
 
 from app.model.embeddings.base_embedding import BaseEmbeddingModel
+from app.model.embeddings.word2vec_embedding import Word2VecEmbedding
 from app.model.similarity.base_similarity import BaseSimilarityMetric
+from app.model.similarity.cosine_similarity import CosineSimilarity
 from app.services.movie_service import MovieService
 
 
 def get_embedding_model() -> BaseEmbeddingModel:
     """Returns the embedding model"""
-    return None
+    return Word2VecEmbedding()
 
 
 def get_similarity_service() -> BaseSimilarityMetric:
     """Returns the similarity metric"""
-    return None
+    return CosineSimilarity()
 
 
 def get_movie_service(embedding_model=Depends(get_embedding_model),
