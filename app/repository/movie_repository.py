2626c468ae932ae4046c200f2e51c06320a5100c
Albert Salles <albert.salles@mosaicfactor.com>
2025-02-16 23:36:54 +0100

    Fix: Convert release_date to datetime col

--- app/repository/movie_repository.py
+++ app/repository/movie_repository.py
@@ -6,5 +6,6 @@ from app.model.embeddings.base_embedding import BaseEmbeddingModel
 def get_movies_dataset(embedding_model: BaseEmbeddingModel) -> pd.DataFrame:
     """Loads the dataset from the movies.csv file"""
     df = pd.read_csv("app/repository/dataset/movies.csv", index_col=0)
+    df["release_date"] = pd.to_datetime(df["release_date"], format="%Y-%m-%d")
     df["embedding"] = df["overview"].apply(lambda x: embedding_model.encode_text(str(x)))
     return df
