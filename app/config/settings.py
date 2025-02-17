45657c67bd29829dfdc7a0c53df18328f34ff9bc
Albert Salles <albert.salles@mosaicfactor.com>
2025-02-17 11:18:14 +0100

    Change: Default to word2vec

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,5 +4,5 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    EMBEDDING_MODEL: str = os.getenv("EMBEDDING_MODEL", "transformer")
+    EMBEDDING_MODEL: str = os.getenv("EMBEDDING_MODEL", "word2vec")
     SIMILARITY_METRIC: str = os.getenv("SIMILARITY_METRIC", "cosine")
