f7c781df0b1373466e44a61c0c88dba74f15ca76
Albert Salles <albert.salles@mosaicfactor.com>
2025-02-17 10:54:57 +0100

    Fix: Fillna for movies without description

--- app/repository/movie_repository.py
+++ app/repository/movie_repository.py
@@ -7,5 +7,7 @@ def get_movies_dataset(embedding_model: BaseEmbeddingModel) -> pd.DataFrame:
     """Loads the dataset from the movies.csv file"""
     df = pd.read_csv("app/repository/dataset/movies.csv", index_col=0)
     df["release_date"] = pd.to_datetime(df["release_date"], format="%Y-%m-%d")
-    df["embedding"] = df["overview"].apply(lambda x: embedding_model.encode_text(str(x)))
+    df["embedding"] = df.apply(lambda row: embedding_model.encode_text(str(row["title"])) if pd.isna(
+        row["overview"]) else embedding_model.encode_text(str(row["overview"])), axis=1)
+    df["overview"] = df["overview"].fillna("")
     return df
